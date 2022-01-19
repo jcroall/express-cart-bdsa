d0d4747fd40d5808be3ec7392ed3f891588791e6
James Croall <jcroall@pobox.com>
2022-01-19 01:45:37 +0000

    Initial commit

---
+++ env-full.sh
@@ -0,0 +1,12 @@
+export GITHUB_API_URL="https://api.github.com"
+export GITHUB_TOKEN="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"
+export GITHUB_REPOSITORY="jcroall/express-cart-demo-day"
+export GITHUB_SHA="a3bcb415802ca95787ab6a4443363ecaaa23bf27"
+export GITHUB_HEAD_REF="dev1"
+export GITHUB_BASE_REF="master"
+export GITHUB_REF="refs/pull/1/merge"
+
+export COVERITY_URL="http://coverity-demo:8080"
+export COV_USER="siguser"
+export COVERITY_PASSPHRASE="SIGpass8!"
+export COVERITY_STREAM_NAME=express-cart-demo-day-master
